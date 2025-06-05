c3d91064d9af879a9b252166be34eeaacfef5fbb
sharan <sharansalokye@gmail.com>
2025-06-05 14:44:48 +0530

    Add CMake build and implement return book wizard

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
+# Integrated Library System
 
-Integrated Library System 
-using C++ and Qt
+This project is a Qt based application for managing a small library. It was originally built with qmake but now provides a modern **CMake** build system.
 
+## Building
+
+```bash
+cmake -S . -B build
+cmake --build build
+```
+
+The resulting executable is `build/ils`. Pass a custom database path as the first argument or place `ils.sqlite` next to the executable.
+
+```bash
+./ils /path/to/ils.sqlite
+```
--- wizards/returnbookwizarad.cpp
+++ wizards/returnbookwizarad.cpp
@@ -1,12 +1,30 @@
+#include <QSqlRecord>
 #include "returnbookwizarad.h"
 
 ReturnBookWizarad::ReturnBookWizarad(QWidget *parent) :
     QWizard(parent)
 {
-    addPage(new SelectBookPage);
+    addPage(&sbp);
+    addPage(&rfp);
+    setButtonText(QWizard::FinishButton, "Return Book");
 }
+
+void ReturnBookWizarad::setMemberRoll(const QString &roll)
+{
+    m_memberRoll = roll;
+    sbp.setMember(roll);
+}
+
 void ReturnBookWizarad::accept()
 {
+    QString bookNo = field("bookNo").toString();
+    QSqlQuery q;
+    q.exec(QString("SELECT memberID FROM members WHERE roll='%1'").arg(m_memberRoll));
+    if(q.next()) {
+        QString memberID = q.value(0).toString();
+        QSqlQuery del(QString("DELETE FROM issue WHERE memberID=%1 AND bookNo=%2").arg(memberID).arg(bookNo));
+        QSqlQuery upd(QString("UPDATE bookNumbers SET issued=0 WHERE bookNo=%1").arg(bookNo));
+    }
     QDialog::accept();
 }
 
@@ -20,6 +38,11 @@ SelectBookPage::SelectBookPage(QWidget *parent) :
     branchLabel.setText("Branch");
     booksTaken.setText("Books Taken");
 
+    roll.setReadOnly(true);
+    name.setReadOnly(true);
+    branch.setReadOnly(true);
+    selectedBookEdit.setVisible(false);
+
     layout.addWidget(&rollLabel,0,0);
     layout.addWidget(&roll,0,1);
     layout.addWidget(&nameLabel,0,2);
@@ -27,21 +50,50 @@ SelectBookPage::SelectBookPage(QWidget *parent) :
     layout.addWidget(&branchLabel,1,0);
     layout.addWidget(&branch,1,1);
     layout.addWidget(&booksTaken,2,0);
-    layout.addWidget(&issuedBooksTable,3,0,0,4);
-
+    layout.addWidget(&issuedBooksTable,3,0,1,4);
 
     setLayout(&layout);
+
+    registerField("bookNo", &selectedBookEdit);
+    connect(&issuedBooksTable, &QTableView::clicked, this, &SelectBookPage::rowSelected);
 }
 
-
-void SelectBookPage::getStudentInfo()
+void SelectBookPage::setMember(const QString &rollNo)
 {
+    roll.setText(rollNo);
+    QSqlQuery q;
+    q.prepare("SELECT memberID, Name, Branch FROM members WHERE roll=?");
+    q.addBindValue(rollNo);
+    if(q.exec() && q.next()) {
+        memberID = q.value(0).toString();
+        name.setText(q.value(1).toString());
+        branch.setText(q.value(2).toString());
+        model.setTable("issue");
+        model.setFilter(QString("memberID='%1'").arg(memberID));
+        model.select();
+        issuedBooksTable.setModel(&model);
+        issuedBooksTable.hideColumn(0);
+        issuedBooksTable.hideColumn(3);
+    }
+}
 
+void SelectBookPage::rowSelected(const QModelIndex &index)
+{
+    QSqlRecord r = model.record(index.row());
+    selectedBookEdit.setText(r.value("bookNo").toString());
+}
+
+int SelectBookPage::nextId() const
+{
+    return 1;
 }
 
 //====================================//
 ReturnFinalPage::ReturnFinalPage(QWidget *parent):
     QWizardPage(parent)
 {
-
+    setTitle("Return Book");
+    infoLabel.setText("Press Finish to return the selected book.");
+    layout.addWidget(&infoLabel);
+    setLayout(&layout);
 }
